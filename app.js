269a128eae3aeb34cf6a8e70255611a09bc1982c
Leslie Lawson <lawsonjleslie@gmail.com>
2020-08-09 11:03:59 -0400

    app.js done generate html page but having a problem with the ID and the email category

--- app.js
+++ app.js
@@ -5,142 +5,113 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
 const employees = [];
-init();
 
-// Write code to use inquirer to gather information about the development team members,
-function init(){
-    inquirer.prompt(
-    [{
-        type:"input",
-        message:"Enter the team member's name",
-        name: "name",
-    },
-    {
-        type:"list",
-        message:"Enter the team member's role",
-        name:"role",
-        choices: [
-            "Manager",
-            "Engineer",
-            "Intern"
-        ],
-    },
-    {
-        type:"input",
-        message:"Enter the team member's ID",
-        name:"ID", 
-    },
-    {
-        type:"email",
-        message:"Enter the team member's email address",
-        name:"email"
-    }
-])
-    
+function addMember() {
+   
+   inquirer.prompt([{
+         message: "Enter team member's name: ",
+         name: "name"
+      },
+      {
+         type: "list",
+         message: "Select team member's role: ",
+         choices: [
+               "Manager",
+               "Engineer",
+               "Intern"
+         ],
+         name: "role"
+      },
+      {
+         message: "Enter team member's ID: ",
+         name: "id"
+      },
+      {
+         message: "Enter team member's email address: ",
+         name: "email"
+      }
+   ]).then(function ({ name, role, id, email }) {
 
+         let infoType;
 
-// and to create objects for each team member (using the correct classes as blueprints!)
+         if (role === "Manager") {
+            infoType = "office phone number";
+         } else if (role === "Engineer") {
+            infoType = "GitHub username";
+         } else {
+            infoType = "school name";
+         }
 
-.then(function ({ name, role, id, email }) {
+         inquirer.prompt([
+            {
+               message: `Enter team member's ${infoType}: `,
+               name: "info"
+            },
+            {
+               type: "list",
+               message: "Would you like to add more team members?",
+               choices: [
+                     "yes",
+                     "no"
+               ],
+               name: "moreMembers"
+            }
+         ]).then(function ({ info, moreMembers }) {
+               let newMember;
 
-    let infoType;
+               if (role === "Engineer") {
+                  newMember = new Engineer(name, id, email, info);
+               } else if (role === "Intern") {
+                  newMember = new Intern(name, id, email, info);
+               } else {
+                  newMember = new Manager(name, id, email, info);
+               }
 
-    if (role === "Manager") {
-       infoType = "office phone number";
-    } else if (role === "Engineer") {
-       infoType = "GitHub username";
-    } else {
-       infoType = "school name";
-    }
-    inquirer.prompt([
-        {
-           message: `Enter team member's ${infoType}: `,
-           name: "info"
-        },
-        {
-           type: "list",
-           message: "Would you like to add more team members?",
-           choices: [
-                 "yes",
-                 "no"
-           ],
-           name: "moreMembers"
-        }
-     ]).then(function ({ info, moreMembers }) {
-           let newMember;
-
-           if (role === "Engineer") {
-              newMember = new Engineer(name, id, email, info);
-           } else if (role === "Intern") {
-              newMember = new Intern(name, id, email, info);
-           } else {
-              newMember = new Manager(name, id, email, info);
-           }
-
-           employees.push(newMember);
-           
-           if (moreMembers === "yes") {
-              addMember();
-           } else {
-              createHtml();
-           }
-     });
-
-});
+               employees.push(newMember);
+               
+               if (moreMembers === "yes") {
+                  addMember();
+               } else {
+                  createHtml();
+               }
+         });
+   
+   });
 }
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+
 function createHtml() {
 
-    const htmlPage = render(employees);
- 
-    if (!fs.existsSync(OUTPUT_DIR)){
-       console.log(`'output' folder does not exist. Creating it now ...`)
-       fs.mkdirSync(OUTPUT_DIR);
-    } else {
-       console.log(`output folder already exists.`);
-    }
- 
-    fs.writeFile(outputPath, htmlPage, function (err) {
-       if (err) {
-            console.log(`Failed to create file - ${outputPath}. Error - ${err}`);
-       } else {
-          console.log(`Successfully created html file - ${outputPath}`);
-       }      
-    });
- 
- }
- 
- function main(){
- 
-    addMember();
- 
- }
- main();
+   const htmlPage = render(employees);
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
+   if (!fs.existsSync(OUTPUT_DIR)){
+      console.log(`'output' folder does not exist. Creating it now ...`)
+      fs.mkdirSync(OUTPUT_DIR);
+   } else {
+      console.log(`output folder already exists.`);
+   }
 
+   fs.writeFile(outputPath, htmlPage, function (err) {
+      if (err) {
+           console.log(`Failed to create file - ${outputPath}. Error - ${err}`);
+      } else {
+         console.log(`Successfully created html file - ${outputPath}`);
+      }      
+   });
 
+}
 
+function main(){
 
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+   console.log("Get team's members information and generate an HTML file");
+   console.log("-------------------------------------------------------------")
+   addMember();
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+}
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! 
+main();
